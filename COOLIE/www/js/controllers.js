ec90a4b24d680889b8a4f1e8548427021f609dcb
Arpit Nandwani <arpit.nandwani@gmail.com>
2017-03-26 15:27:40 +0530

    Beta 1.0.0

---
+++ COOLIE/www/js/controllers.js
@@ -0,0 +1,66 @@
+angular.module('app.controllers', [])
+  
+.controller('homeCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('cOOLIECtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('enterTicketIDCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('ticketInfoCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('promotionsCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('medicalEmergencyCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('helpAndSupportCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('reportLostBaggageCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+ 
---
+++ COOLIE/www/js/routes.js
@@ -0,0 +1,93 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+  
+
+      .state('cOOLIE.home', {
+    url: '/page1',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/home.html',
+        controller: 'homeCtrl'
+      }
+    }
+  })
+
+  .state('cOOLIE', {
+    url: '/side-menu21',
+    templateUrl: 'templates/cOOLIE.html',
+    controller: 'cOOLIECtrl'
+  })
+
+  .state('cOOLIE.enterTicketID', {
+    url: '/page5',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/enterTicketID.html',
+        controller: 'enterTicketIDCtrl'
+      }
+    }
+  })
+
+  .state('cOOLIE.ticketInfo', {
+    url: '/page6',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/ticketInfo.html',
+        controller: 'ticketInfoCtrl'
+      }
+    }
+  })
+
+  .state('cOOLIE.promotions', {
+    url: '/page7',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/promotions.html',
+        controller: 'promotionsCtrl'
+      }
+    }
+  })
+
+  .state('cOOLIE.medicalEmergency', {
+    url: '/page8',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/medicalEmergency.html',
+        controller: 'medicalEmergencyCtrl'
+      }
+    }
+  })
+
+  .state('cOOLIE.helpAndSupport', {
+    url: '/page9',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/helpAndSupport.html',
+        controller: 'helpAndSupportCtrl'
+      }
+    }
+  })
+
+  .state('cOOLIE.reportLostBaggage', {
+    url: '/page10',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/reportLostBaggage.html',
+        controller: 'reportLostBaggageCtrl'
+      }
+    }
+  })
+
+$urlRouterProvider.otherwise('/side-menu21/page1')
+
+  
+
+});
