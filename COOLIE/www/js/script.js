34fbd9ec0377943e36690ae03b4793f0272cf7e1
Arpit Nandwani <arpit.nandwani@gmail.com>
2017-03-31 19:34:36 +0530

    Factored function out

--- COOLIE/www/js/script.js
+++ COOLIE/www/js/script.js
@@ -1,7 +1,12 @@
+function addtckt(tcktid){
+	alert(tcktid);
+}
+
 main.controller("BarScanController", function($scope, $cordovaBarcodeScanner) {
     $scope.scanBarcode = function() {
         $cordovaBarcodeScanner.scan().then(function(imageData) {
-            alert(imageData.text);
+            //alert(imageData.text);
+			addtckt(imageData.text);
             console.log("Barcode Format -> " + imageData.format);
             console.log("Cancelled -> " + imageData.cancelled);
         }, function(error) {
@@ -13,6 +18,7 @@ main.controller("BarScanController", function($scope, $cordovaBarcodeScanner) {
 
 main.controller("ticket", function($scope){
     $scope.enterid = function(){
-        alert($scope.ticketid);
+        //alert($scope.ticketid);
+		addtckt($scope.ticketid);
     };
 });
